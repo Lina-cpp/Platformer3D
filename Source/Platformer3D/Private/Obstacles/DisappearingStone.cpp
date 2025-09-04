5a179558c29d530218c624a6673bd8f44dabd0bf
Lina <107427736+Lina-cpp@users.noreply.github.com>
2025-09-04 12:31:49 +0200

    Disappearing stones - obstacle

--- Source/Platformer3D/Private/Obstacles/DisappearingStone.cpp
+++ Source/Platformer3D/Private/Obstacles/DisappearingStone.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Obstacles/DisappearingStone.h"
+
+#include "Characters/CharacterBase.h"
 #include "Components/BoxComponent.h"
 
 ADisappearingStone::ADisappearingStone()
@@ -12,20 +14,83 @@ ADisappearingStone::ADisappearingStone()
 		Mesh->SetupAttachment(GetRootComponent());
 	BoxComp = CreateDefaultSubobject<UBoxComponent>("BoxComp");
 		BoxComp->SetupAttachment(Mesh);
+	
+	BoxComp->OnComponentBeginOverlap.AddDynamic(this, &ADisappearingStone::BoxCompBeginOverlap);
+	BoxComp->OnComponentEndOverlap.AddDynamic(this, &ADisappearingStone::BoxCompEndOverlap);
 
+	UMaterialInstanceDynamic* DynMaterial = Mesh->CreateDynamicMaterialInstance(0, Mesh->GetMaterial(0));
 }
 
 
 void ADisappearingStone::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
 
+}
 
 void ADisappearingStone::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
+
+
+/* Box Overlaps */
+	//BeginOverlap
+void ADisappearingStone::BoxCompBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//check if player can stand on rock (is visible and has collision)
+	if (!bCanStandOnRock) return;
+	//Cast to check if actor on stone is player
+	if (ACharacterBase* Char = Cast<ACharacterBase>(OtherActor))
+	{
+		GetWorldTimerManager().ClearTimer(StandOnTimer); //Clear Timer, just in case
+		//Call Timer for 2 sec and then call its function
+		GetWorldTimerManager().SetTimer(StandOnTimer,this, &ADisappearingStone::PlayerOnStone, StandOnTime, false);
+		//Call function from BP
+		BeginOverlap_Implementation();
+	}
+}
+
+
+/* Timers */
+void ADisappearingStone::PlayerOnStone()
+{
+	Mesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	bCanStandOnRock = false;
+}
+
+void ADisappearingStone::BeginOverlap_Implementation()
+{
+	
+}
+
+
+
+	//End Overlap
+void ADisappearingStone::BoxCompEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+
+	if (ACharacterBase* Char = Cast<ACharacterBase>(OtherActor))
+	{
+		GetWorldTimerManager().ClearTimer(StandOnTimer);
+		GetWorldTimerManager().SetTimer(DisappearTimer, this, &ADisappearingStone::StoneDisappeared, ReAppearTime, false);
+	}
+}
+
+void ADisappearingStone::StoneDisappeared()
+{
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	bCanStandOnRock = true;
+	EndOverlap_Implementation();
+}
+
+
+void ADisappearingStone::EndOverlap_Implementation()
+{
+	
+}
+
 
--- Source/Platformer3D/Public/Obstacles/DisappearingStone.h
+++ Source/Platformer3D/Public/Obstacles/DisappearingStone.h
@@ -2,11 +2,14 @@
 
 #pragma once
 
+#include <rapidjson/internal/meta.h>
+
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "DisappearingStone.generated.h"
 
 class UBoxComponent;
+class ACharacterBase;
 
 UCLASS()
 class PLATFORMER3D_API ADisappearingStone : public AActor
@@ -15,16 +18,53 @@ class PLATFORMER3D_API ADisappearingStone : public AActor
 	
 public:	
 	ADisappearingStone();
-
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UStaticMeshComponent* Mesh;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UBoxComponent* BoxComp;
 
+	bool bCanStandOnRock = true;
+
 protected:
 	virtual void BeginPlay() override;
+	
 
+	FTimerHandle StandOnTimer;
+	UFUNCTION()
+	void PlayerOnStone();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float StandOnTime = 2.0f;
+	
+	FTimerHandle DisappearTimer;
+	void StoneDisappeared();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ReAppearTime = 4.0f;
+
+/* Box Overlaps */	
+	UFUNCTION(BlueprintCallable)
+	void BoxCompBeginOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult);
+
+	UFUNCTION(BlueprintCallable)
+	void BoxCompEndOverlap(UPrimitiveComponent* OverlappedComp,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void BeginOverlap();
+	UFUNCTION(BlueprintNativeEvent)
+	void EndOverlap();
+	
+	
+	
 public:	
 	virtual void Tick(float DeltaTime) override;
 
